905c71df9efc14e0bbf02620f73cc00a65ad74dc
wangtao <hdjtdxwt@126.com>
2020-10-30 11:40:39 +0800

    添加第二个按钮

--- app/src/main/java/com/hdjtdxwt/gitdemo/MainActivity.java
+++ app/src/main/java/com/hdjtdxwt/gitdemo/MainActivity.java
@@ -3,14 +3,28 @@ package com.hdjtdxwt.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn1;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn2:{
+                Toast.makeText(this, "测试2", Toast.LENGTH_SHORT).show();
+            }break;
+        }
     }
 }
